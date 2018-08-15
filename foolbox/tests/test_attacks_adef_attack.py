e339e0f818ee5b50339ae4fbf0fa2ec8d8da02bc
Evgenia Rusak <rusak@fzi.de>
2018-08-15 18:27:33 +0200

    fixed formatting in docu, hopefully. Added coverage for pytorch.

--- foolbox/tests/test_attacks_adef_attack.py
+++ foolbox/tests/test_attacks_adef_attack.py
@@ -3,6 +3,14 @@ import numpy as np
 from foolbox.attacks import ADefAttack as Attack
 
 
+def test_attack_pytorch(bn_adversarial_pytorch):
+    adv = bn_adversarial_pytorch
+    attack = Attack()
+    attack(adv)
+    assert adv.image is not None
+    assert adv.distance.value < np.inf
+
+
 def test_attack(bn_adversarial):
     adv = bn_adversarial
     attack = Attack()
@@ -25,14 +33,6 @@ def test_targeted_attack(bn_targeted_adversarial):
     attack(adv)
     assert adv.image is None
     assert adv.distance.value == np.inf
-
-
-def test_subsample(bn_adversarial):
-    adv = bn_adversarial
-    attack = Attack()
-    attack(adv, subsample=5)
-    assert adv.image is not None
-    assert adv.distance.value < np.inf
 
 
 def test_attack_impossible(bn_impossible):
